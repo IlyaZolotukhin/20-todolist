e46613b840888ce9dde47ccff83b3b4a680b60dc
IlyaZolotuhin <1zolotyhin1@gmail.com>
2023-12-01 16:44:52 +0700

    Initial commit

---
+++ src/features/TodolistsList/ui/Todolist/TodolistTitle/TodolistTitle.tsx
@@ -0,0 +1,38 @@
+import React, {useCallback} from 'react';
+import {EditableSpan} from "common/components";
+import {IconButton} from "@mui/material";
+import {Delete} from "@mui/icons-material";
+import {useActions} from "common/hooks";
+import {TodolistDomainType, todolistsThunks} from "features/TodolistsList/model/todolists/todolistsSlice";
+
+type Props = {
+    todolist: TodolistDomainType;
+};
+
+const TodolistTitle = ({todolist}:Props) => {
+    const {entityStatus, id, title} = todolist
+
+    const {removeTodolist, changeTodolistTitle} = useActions(todolistsThunks)
+
+    const removeTodolistHandler = () => {
+        removeTodolist(id);
+    };
+
+    const changeTodolistTitleHandler = useCallback(
+        (title: string) => {
+            changeTodolistTitle({ id, title });
+        },
+        [id],
+    );
+
+    return (
+        <h3>
+            <EditableSpan value={title} onChange={changeTodolistTitleHandler} />
+            <IconButton onClick={removeTodolistHandler} disabled={entityStatus === "loading"}>
+                <Delete />
+            </IconButton>
+        </h3>
+    );
+};
+
+export default TodolistTitle;
