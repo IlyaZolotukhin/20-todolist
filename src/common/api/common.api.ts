503b2652c2b54622bc47e47f83cce3c1e32b7519
IlyaZolotuhin <1zolotyhin1@gmail.com>
2023-12-06 11:44:41 +0700

    06.12.23 production commit

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "cfe2881d-c17b-4a41-8a8a-f9d95878b4cc",
+    "API-KEY": "672a3475-5358-46e1-b5c9-5ccc4db7abe6",
   },
 });
