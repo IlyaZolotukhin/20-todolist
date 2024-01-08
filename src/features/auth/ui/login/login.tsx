1b111597a88880731f9f465ac8ffdd26d1448d68
IlyaZolotuhin <1zolotyhin1@gmail.com>
2024-01-08 12:16:27 +0700

    08.01.23 production commit

--- src/features/auth/ui/login/login.tsx
+++ src/features/auth/ui/login/login.tsx
@@ -18,13 +18,7 @@ export const Login = () => {
         <form onSubmit={formik.handleSubmit}>
           <FormControl>
             <FormLabel>
-              <p>
-                To log in get registered{" "}
-                <a href={"https://social-network.samuraijs.com/"} target={"_blank"} rel="noreferrer">
-                  here
-                </a>
-              </p>
-              <p>or use common test account credentials:</p>
+              <p>use common test account credentials:</p>
               <p> Email: free@samuraijs.com</p>
               <p>Password: free</p>
             </FormLabel>
